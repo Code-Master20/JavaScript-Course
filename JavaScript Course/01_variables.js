ea69fddad8cf71930900664113af371ae436972a
Sahidur Miah <sahidurmiah201920@gmail.com>
2025-06-23 10:25:17 +0000

    learning variables

---
+++ JavaScript Course/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 2431;
+let userName = "Sahidur Miah";
+let accountEmail = "sahidurmiah@gmail.com";
+var accountPassword = "8537236";
+accountCity = "Mahendraganj";
+
+accountEmail = "rakibulmiah@gmail.com";
+console.log(accountEmail);
+console.log(accountId);
